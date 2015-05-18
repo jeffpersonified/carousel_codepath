f4e1dc7e528a9273bbde6a55bc4645acf4896939
Jeff Smith <jeff.matthew.smith@gmail.com>
2015-05-18 12:27:05 -0700

    Add keyboard animation functions

--- Carousel/LoginViewController.swift
+++ Carousel/LoginViewController.swift
@@ -10,12 +10,36 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var loginInstructionImageView: UIImageView!
+    @IBOutlet weak var textFieldsView: UIView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var authenticationButtonsView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func keyboardWillShow(notification: NSNotification!) {
+
+        var userInfo = notification.userInfo!
+        var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue().size
+        
+        loginInstructionImageView.hidden = true
+        UIView.animateWithDuration(0.4, animations: {
+            self.textFieldsView.frame.origin.y = 67
+            self.authenticationButtonsView.frame.origin.y = 568 - kbSize.height - 113
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        loginInstructionImageView.hidden = true
     }
     
     @IBAction func backButtonDidPress(sender: AnyObject) {
